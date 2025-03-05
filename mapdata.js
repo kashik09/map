7c49ae1886335ff7ab3e3617b8d590859e99ce67
Kashi Kweyu <kashiku789@gmail.com>
2025-03-05 14:05:36 +0300

    initial commit

---
+++ mapdata.js
@@ -0,0 +1,467 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+    width: "",
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    state_description: "",
+    state_color: "#bdbdbd",
+    state_hover_color: "#3b729f",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#ffffff",
+    label_hover_color: "#ffffff",
+    label_size: 16,
+    label_font: "Arial",
+    label_display: "auto",
+    label_scale: "yes",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "no",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    "0": {},
+    KE01: {
+      name: "Mombasa",
+      zoomable: "no"
+    },
+    KE02: {
+      name: "Kwale",
+      zoomable: "no"
+    },
+    KE03: {
+      name: "Kilifi",
+      zoomable: "no"
+    },
+    KE04: {
+      name: "Tana River",
+      zoomable: "no"
+    },
+    KE05: {
+      name: "Lamu",
+      zoomable: "no"
+    },
+    KE06: {
+      name: "Taita Taveta",
+      zoomable: "no"
+    },
+    KE07: {
+      name: "Garissa",
+      zoomable: "no"
+    },
+    KE08: {
+      name: "Wajir",
+      zoomable: "no"
+    },
+    KE09: {
+      name: "Mandera",
+      zoomable: "no"
+    },
+    KE10: {
+      name: "Marsabit",
+      zoomable: "no"
+    },
+    KE11: {
+      name: "Isiolo",
+      zoomable: "no"
+    },
+    KE12: {
+      name: "Meru",
+      zoomable: "no",
+      color: "#0eb6cc"
+    },
+    KE13: {
+      name: "Tharaka-Nithi",
+      zoomable: "no"
+    },
+    KE14: {
+      name: "Embu",
+      zoomable: "no"
+    },
+    KE15: {
+      name: "Kitui",
+      zoomable: "no"
+    },
+    KE16: {
+      name: "Machakos",
+      zoomable: "no",
+      color: "#0eb6cc"
+    },
+    KE17: {
+      name: "Makueni",
+      zoomable: "no"
+    },
+    KE18: {
+      name: "Nyandarua",
+      zoomable: "no"
+    },
+    KE19: {
+      name: "Nyeri",
+      zoomable: "no"
+    },
+    KE20: {
+      name: "Kirinyaga",
+      zoomable: "no"
+    },
+    KE21: {
+      name: "Murang'a",
+      zoomable: "no"
+    },
+    KE22: {
+      name: "Kiambu",
+      zoomable: "no",
+      color: "#0eb6cc"
+    },
+    KE23: {
+      name: "Turkana",
+      zoomable: "no"
+    },
+    KE24: {
+      name: "West Pokot",
+      zoomable: "no"
+    },
+    KE25: {
+      name: "Samburu",
+      zoomable: "no"
+    },
+    KE26: {
+      name: "Trans Nzoia",
+      zoomable: "no"
+    },
+    KE27: {
+      name: "Uasin Gishu",
+      zoomable: "no"
+    },
+    KE28: {
+      name: "Elgeyo-Marakwet",
+      zoomable: "no"
+    },
+    KE29: {
+      name: "Nandi",
+      zoomable: "no"
+    },
+    KE30: {
+      name: "Baringo",
+      zoomable: "no"
+    },
+    KE31: {
+      name: "Laikipia",
+      zoomable: "no",
+      color: "#0eb6cc"
+    },
+    KE32: {
+      name: "Nakuru",
+      zoomable: "no"
+    },
+    KE33: {
+      name: "Narok",
+      zoomable: "no"
+    },
+    KE34: {
+      name: "Kajiado",
+      zoomable: "no"
+    },
+    KE35: {
+      name: "Kericho",
+      zoomable: "no"
+    },
+    KE36: {
+      name: "Bomet",
+      zoomable: "no"
+    },
+    KE37: {
+      name: "Kakamega",
+      zoomable: "no",
+      color: "#0eb6cc"
+    },
+    KE38: {
+      name: "Vihiga",
+      zoomable: "no"
+    },
+    KE39: {
+      name: "Bungoma",
+      zoomable: "no"
+    },
+    KE40: {
+      name: "Busia",
+      zoomable: "no"
+    },
+    KE41: {
+      name: "Siaya",
+      zoomable: "no"
+    },
+    KE42: {
+      name: "Kisumu",
+      zoomable: "no"
+    },
+    KE43: {
+      name: "Homa Bay",
+      zoomable: "no",
+      color: "#0eb6cc"
+    },
+    KE44: {
+      name: "Migori",
+      zoomable: "no",
+      color: "#0eb6cc"
+    },
+    KE45: {
+      name: "Kisii",
+      zoomable: "no"
+    },
+    KE46: {
+      name: "Nyamira",
+      zoomable: "no"
+    },
+    KE47: {
+      name: "Nairobi",
+      zoomable: "no",
+      color: "#0eb6cc"
+    }
+  },
+  locations: {},
+  labels: {
+    KE01: {
+      name: "Mombasa",
+      parent_id: "KE01"
+    },
+    KE02: {
+      name: "Kwale",
+      parent_id: "KE02"
+    },
+    KE03: {
+      name: "Kilifi",
+      parent_id: "KE03"
+    },
+    KE04: {
+      name: "Tana River",
+      parent_id: "KE04"
+    },
+    KE05: {
+      name: "Lamu",
+      parent_id: "KE05"
+    },
+    KE06: {
+      name: "Taita Taveta",
+      parent_id: "KE06"
+    },
+    KE07: {
+      name: "Garissa",
+      parent_id: "KE07"
+    },
+    KE08: {
+      name: "Wajir",
+      parent_id: "KE08"
+    },
+    KE09: {
+      name: "Mandera",
+      parent_id: "KE09"
+    },
+    KE10: {
+      name: "Marsabit",
+      parent_id: "KE10"
+    },
+    KE11: {
+      name: "Isiolo",
+      parent_id: "KE11"
+    },
+    KE12: {
+      name: "Meru",
+      parent_id: "KE12"
+    },
+    KE13: {
+      name: "Tharaka-Nithi",
+      parent_id: "KE13"
+    },
+    KE14: {
+      name: "Embu",
+      parent_id: "KE14"
+    },
+    KE15: {
+      name: "Kitui",
+      parent_id: "KE15"
+    },
+    KE16: {
+      name: "Machakos",
+      parent_id: "KE16"
+    },
+    KE17: {
+      name: "Makueni",
+      parent_id: "KE17"
+    },
+    KE18: {
+      name: "Nyandarua",
+      parent_id: "KE18"
+    },
+    KE19: {
+      name: "Nyeri",
+      parent_id: "KE19"
+    },
+    KE20: {
+      name: "Kirinyaga",
+      parent_id: "KE20"
+    },
+    KE21: {
+      name: "Murang'a",
+      parent_id: "KE21"
+    },
+    KE22: {
+      name: "Kiambu",
+      parent_id: "KE22"
+    },
+    KE23: {
+      name: "Turkana",
+      parent_id: "KE23"
+    },
+    KE24: {
+      name: "West Pokot",
+      parent_id: "KE24"
+    },
+    KE25: {
+      name: "Samburu",
+      parent_id: "KE25"
+    },
+    KE26: {
+      name: "Trans Nzoia",
+      parent_id: "KE26"
+    },
+    KE27: {
+      name: "Uasin Gishu",
+      parent_id: "KE27"
+    },
+    KE28: {
+      name: "Elgeyo-Marakwet",
+      parent_id: "KE28"
+    },
+    KE29: {
+      name: "Nandi",
+      parent_id: "KE29"
+    },
+    KE30: {
+      name: "Baringo",
+      parent_id: "KE30"
+    },
+    KE31: {
+      name: "Laikipia",
+      parent_id: "KE31"
+    },
+    KE32: {
+      name: "Nakuru",
+      parent_id: "KE32"
+    },
+    KE33: {
+      name: "Narok",
+      parent_id: "KE33"
+    },
+    KE34: {
+      name: "Kajiado",
+      parent_id: "KE34"
+    },
+    KE35: {
+      name: "Kericho",
+      parent_id: "KE35"
+    },
+    KE36: {
+      name: "Bomet",
+      parent_id: "KE36"
+    },
+    KE37: {
+      name: "Kakamega",
+      parent_id: "KE37"
+    },
+    KE38: {
+      name: "Vihiga",
+      parent_id: "KE38"
+    },
+    KE39: {
+      name: "Bungoma",
+      parent_id: "KE39"
+    },
+    KE40: {
+      name: "Busia",
+      parent_id: "KE40"
+    },
+    KE41: {
+      name: "Siaya",
+      parent_id: "KE41"
+    },
+    KE42: {
+      name: "Kisumu",
+      parent_id: "KE42"
+    },
+    KE43: {
+      name: "Homa Bay",
+      parent_id: "KE43"
+    },
+    KE44: {
+      name: "Migori",
+      parent_id: "KE44"
+    },
+    KE45: {
+      name: "Kisii",
+      parent_id: "KE45"
+    },
+    KE46: {
+      name: "Nyamira",
+      parent_id: "KE46"
+    },
+    KE47: {
+      name: "Nairobi",
+      parent_id: "KE47"
+    }
+  },
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
